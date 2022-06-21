e349c9fb34b856f2edeff79969a3bb92bb192433
victoria mwangi <victoriamwangi021@gmail.com>
2022-06-21 14:35:08 +0300

    feat: update profile

--- amechanic/urls.py
+++ amechanic/urls.py
@@ -3,7 +3,11 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('', views.index, name='home')   
+    path('', views.home, name='home'),
+    path('profile/<username>/settings', views.update_profile, name='update_profile'),
+    path('profile/<username>', views.profile, name='profile'),
+    
+    
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
