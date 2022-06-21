4dc87ffad270a12c8b0b6669674f0b3bbe2ba734
victoria mwangi <victoriamwangi021@gmail.com>
2022-06-21 14:49:11 +0300

    feat: profile update

--- amechanic/views.py
+++ amechanic/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect,  get_object_or_404
+from .forms import *
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     
     return render (request, 'home.html')
+
+@login_required(login_url= '/accounts/login/')
+def profile(request, username):
+
+  
+    return render(request, 'profile/profile.html')
+
+@login_required(login_url= '/accounts/login/')
+def update_profile(request, username):
+    user = User.objects.get(username=username)
+    if request.method == 'POST':
+        profileform = ProfileForm(request.POST, request.FILES, instance= request.user.profile)    
+        if profileform.is_valid():           
+            profileform.save()
+        return redirect( 'profile', user.username)
+    else:
+        profileform = ProfileForm(instance = request.user.profile)
+        
+    context ={
+            "profileform": profileform
+    }
+    return render(request, "profile/update_profile.html", context)
+ 
